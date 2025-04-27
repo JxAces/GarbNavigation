804ab1452b09dd55369ceef5a9871527b5234e97
jxaces <jadeaceflor@gmail.com>
2025-04-27 16:42:43 +0800

    fix List of Collection using setOptimizedWaypoints and bin indexing

--- environments.js
+++ environments.js
@@ -1,3 +1,3 @@
 export const GOOGLE_API_KEY = "AIzaSyDXi68r5HkBusMJmuuwzyZ-O1pvsuqZdto";
-export const BACKEND = "http://192.168.1.19:8080/api";
+export const BACKEND = "http://192.168.1.21:8080/api";
     
